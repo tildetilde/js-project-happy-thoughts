f3895b482ca2d5a409857a32cd2d29e7b3d8d7f9
tildetilde <tilde.egebrand@gmail.com>
2025-05-07 11:26:01 +0200

    Created MyLikeCounter

---
+++ src/components/MyLikedThoughts.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Thought } from "../App";
+
+type MyLikedThoughtsProps = {
+  likedThoughtIds: string[];
+  thoughts: Thought[];
+};
+
+export default function MyLikedThoughts({
+  likedThoughtIds = [],
+  thoughts = [],
+}: MyLikedThoughtsProps) {
+  const likedThoughts = thoughts.filter((t) => likedThoughtIds.includes(t.id));
+
+  return (
+    <div className="text-center text-sm text-gray-500" aria-live="polite">
+      You’ve liked {likedThoughts.length}{" "}
+      {likedThoughts.length === 1 ? "thought" : "thoughts"}
+    </div>
+  );
+}
